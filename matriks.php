dac4469d5aa2a994795ec47a868887a0a8e0afd4
aminsochip <44044650+aminsochip@users.noreply.github.com>
2018-10-16 09:52:41 +0700

    Create matriks.php

---
+++ matriks.php
@@ -0,0 +1,13 @@
+<?php
+
+$variable[1] = "Hello";
+$variable[2] = "Mabes TNI";
+
+echo $variable1; echo $variable[2];
+
+echo '<br>';
+
+$variable = array("Hello", "Mabes TNI");
+echo $variable[0];
+echo $variable[1];
+?>
